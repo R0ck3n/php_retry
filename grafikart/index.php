409be74d21b97c732507e466a7c58d7b679dcc77
R0ck3n <r0ck3n@hotmail.com>
2023-05-26 21:06:36 +0200

    favicon

--- grafikart/index.php
+++ grafikart/index.php
@@ -9,9 +9,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg">
     <link rel="stylesheet" href="style.css">
     <script type="module" src="main.js" defer></script>
-    <title>Document</title>
+    <title>Exo PHP</title>
 </head>
 <body>
     <div>
